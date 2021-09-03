a04175ad1de88d9f9fc67a95e20627cc84d87550
Nikit <singhnikit16@gmail.com>
2021-09-04 02:22:47 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# what is this?
+
+Fetches the link of the audio based on the search string from youtube.
+
+
+# Installation:
+
+`npm install yt-audio-link --save`
+
+Then...
+
+```
+const ytml = require("yt-audio-link");
+
+const anyFunction = async () => {
+    const audioLink = await ytml("never gonna give you up");
+    console.log(audioLink);
+} 
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+const ytdl = require('ytdl-core')
+const yts = require('youtube-search-api');
+
+const getAudioUrl = async(searchStr)=>{
+    const data = await yts.GetListByKeyword(searchStr,[false]);
+    let vidId = data['items'][0]['id'];
+    const songInfo = await ytdl.getInfo(`https://www.youtube.com/watch?v=${vidId}`)
+    for(let i in songInfo['formats']){
+        if(songInfo['formats'][i].hasOwnProperty('mimeType')){
+            if(songInfo['formats'][i].mimeType === 'audio/webm; codecs="opus"'){         
+                return songInfo['formats'][i].audioBitrate, songInfo['formats'][i].url
+            }
+        }
+    }
+}
+
+const func = async () => {
+    const song = await getAudioUrl('never gonna give you up')
+    console.log(song)
+}
+
+func();
+
+module.exports.ytml = getAudioUrl;
